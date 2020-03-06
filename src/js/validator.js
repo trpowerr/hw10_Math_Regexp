5a1edea7262485ac29c5788cb8ec62a7fd13d1b4
Tryakin Mikhail <mixazbubl@yandex.ru>
2020-03-06 18:17:25 +0300

    Update validator.js

--- src/js/validator.js
+++ src/js/validator.js
@@ -4,7 +4,7 @@ export class Validator {
     }
 
     validateUsername(userName) {
-        if (userName.search(/(^[a-z+A-Z]+)([\w\-_]*)([a-z+A-Z]$)/) == -1) {
+        if (userName.search(/(^[a-z+A-Z]+)([\w-]*)([a-z+A-Z]$)/) == -1) {
              throw('Wrong user name!');
         } else {
             return userName;
